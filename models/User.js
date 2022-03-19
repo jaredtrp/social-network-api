d69ec630e1cc44cba35f1c65c9d92051920ed460
Jared <jaredtrp@gmail.com>
2022-03-19 13:32:48 -0500

    coded user model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, Model } = require('mongoose');
+const validateEmail = require('../utils/validator.js');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: [validateEmail, 'Please enter a valid email address'],
+      match: [
+        /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/,
+        'Please fill a valid email address',
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
